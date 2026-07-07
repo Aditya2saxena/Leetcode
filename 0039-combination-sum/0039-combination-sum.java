c4fb07cc5cd9710b21d790e74d7c6dabdc373c30
Aditya2saxena <officaladityasaxena@gmail.com>
2026-07-07 10:39:43 +0530

    Time: 2 ms (96.34%), Space: 45.9 MB (33.84%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,40 @@
+import java.util.*;
+
 class Solution {
-    public static void cs(int[] candiates,int target,List<List<Integer>>res,List<Integer>ans,int i){
-        if(i==candiates.length || target<0){
-            return;
-        }
-        if(target==0){
+
+    public static void cs(int[] candidates, int target,
+                          List<List<Integer>> res,
+                          List<Integer> ans,
+                          int i) {
+
+        // Base Case
+        if (target == 0) {
             res.add(new ArrayList<>(ans));
             return;
         }
-        ans.add(candiates[i]);
-        // single inclusion
-        cs(candiates,target-candiates[i],res,ans,i);
-        // multiple inclusion
-        //cs(candiates,target-candiates[i],res,ans,i);
-        // backtrack
-        ans.remove(ans.size()-1);
-        cs(candiates,target,res,ans,i+1);
+
+        if (i == candidates.length || target < 0) {
+            return;
+        }
+
+        // Include current element
+        ans.add(candidates[i]);
+        cs(candidates, target - candidates[i], res, ans, i);
+
+        // Backtrack
+        ans.remove(ans.size() - 1);
+
+        // Exclude current element
+        cs(candidates, target, res, ans, i + 1);
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        List<List<Integer>>res=new ArrayList<>();
-        List<Integer>ans=new ArrayList<>();
-        cs(candidates,target,res,ans,0);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        cs(candidates, target, res, ans, 0);
+
         return res;
     }
 }
