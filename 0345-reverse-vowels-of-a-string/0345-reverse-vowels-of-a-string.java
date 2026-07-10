eba3bfb73b228312f8af2fee6caa5ddc169689c2
Aditya2saxena <officaladityasaxena@gmail.com>
2026-07-10 16:49:37 +0530

    Time: 2 ms (99.28%), Space: 46.7 MB (50.06%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    boolean isVowel(char ch){
+        if(ch=='a'|| ch=='e'||  ch=='i'|| ch=='o'|| ch=='u' || ch=='A'|| ch=='E' || ch=='I' ||  ch=='O' || ch=='U'){
+            return true;
+        }
+        return false;
+    }
+    public String reverseVowels(String s) {
+        char ch[]=s.toCharArray();
+        int i=0;
+        int j=ch.length-1;
+        while(i<j ){
+            while(i<j && !isVowel(ch[i])){
+                i++;
+            }
+            while(i<j && !isVowel(ch[j])){
+                j--;
+            }
+            char temp=ch[i];
+            ch[i]=ch[j];
+            ch[j]=temp;
+            i++;
+            j--;
+
+        }
+        return new String(ch);
+    }
+}
